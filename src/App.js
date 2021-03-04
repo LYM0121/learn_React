f8d65ef3b2104053fc5485f79136128e567f1d91
李奕珉 <liyimin@dayi-air.local>
2021-03-04 22:46:23 +0800

    0304基础数组便利

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Change from "./components/Change";
+let arr = ['vue', 'react', 'angulars']
 
 function App() {
+
   return (
-    <div className={'hello'}>hello,world</div>
+    <div>
+      前端框架列表
+      <Change ></Change>
+      <ul>
+        {arr.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
+    </div>
   );
 }
 
---
+++ src/components/Change.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Change extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        今天天气很 <span>炎热</span>
+      </div>
+    );
+  }
+}
+
+export default Change;
